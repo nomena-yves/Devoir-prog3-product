d2cc1be3ace0057e749ca9d20d7bd27c5f694529
nomena <hei.nomenjanahary@gmail.com>
2025-12-07 23:27:05 +0300

    formation du fichier data

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,5 @@
+insert into product (id,name,price,creation_datime) values (1,"Laptop Dell Xps",4500.00,"2024-01-15-09:30:00");
+insert into product (id,name,price,creation_datime) values (2,"iphone13",5200.00,"2024-02-01-14:10:00");
+insert into product (id,name,price,creation_datime) values (3,"Casque Sony WH1000",890.50,"2024-02-10-16:45:00");
+insert into  product (id,name,price,creation_datime) values (4,"clavier logitech",180.00,"2024-03-05-11:20:00");
+insert into product (id,name,price,creation_datime) values (5,"Ecran Samsung 27",1200.00,"")
