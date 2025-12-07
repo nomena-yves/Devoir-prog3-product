45c9a758c281233df674f9f1d3df4a1743105565
nomena <hei.nomenjanahary@gmail.com>
2025-12-07 13:07:52 +0300

    first commit

---
+++ src/main/java/hei/group/devoirprog3/DevoirProg3Application.java
@@ -0,0 +1,13 @@
+package hei.group.devoirprog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoirProg3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevoirProg3Application.class, args);
+    }
+
+}
---
+++ src/test/java/hei/group/devoirprog3/DevoirProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.group.devoirprog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoirProg3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
