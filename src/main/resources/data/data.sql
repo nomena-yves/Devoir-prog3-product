30ecd3a157f1d0b35da4c6e5bdd1f591f26a33b3
nomena <hei.nomenjanahary@gmail.com>
2025-12-08 08:02:21 +0300

    quellque modification

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -2,4 +2,4 @@ insert into product (id,name,price,creation_datime) values (1,"Laptop Dell Xps",
 insert into product (id,name,price,creation_datime) values (2,"iphone13",5200.00,"2024-02-01-14:10:00");
 insert into product (id,name,price,creation_datime) values (3,"Casque Sony WH1000",890.50,"2024-02-10-16:45:00");
 insert into  product (id,name,price,creation_datime) values (4,"clavier logitech",180.00,"2024-03-05-11:20:00");
-insert into product (id,name,price,creation_datime) values (5,"Ecran Samsung 27",1200.00,"")
+insert into product (id,name,price,creation_datime) values (5,"Ecran Samsung 27",1200.00,"2024-03-18-08:00:00");
