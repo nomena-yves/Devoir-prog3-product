58aa4470d2a4beadcbabd5422179c5f4e4c4e29a
nomena <hei.nomenjanahary@gmail.com>
2025-12-09 23:34:46 +0300

    reponse de la 3eme question

--- src/main/java/hei/group/devoirprog3/DataRedriver/DataRetriever.java
+++ src/main/java/hei/group/devoirprog3/DataRedriver/DataRetriever.java
@@ -60,4 +60,86 @@ public class DataRetriever {
        }
        return products;
     }
+    public List<Product> getProductsByCriteria(String productName, String categoryName, Instant creationMin, Instant creationMax)throws java.sql.SQLException{
+    if (productName!=null){
+        String sql= "SELECT * FROM Product WHERE name ILIKE ?";
+        List<Product> products = new ArrayList<>();
+        try(Connection conn =dbConnection.getConnection();
+        PreparedStatement stmt =conn.prepareStatement(sql);){
+            stmt.setString(1, productName);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                Instant createdAt = rs.getTimestamp("creation_datime").toInstant();
+                Product product = new Product(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        createdAt,
+                        null
+                );
+                products.add(product);
+            }
+            return products;
+        }
+
+    } else if (categoryName!=null) {
+        String sql= "SELECT * FROM Product p inner join product_category c on p.id=c.id WHERE c.name ILIKE ?";
+        List<Product> products = new ArrayList<>();
+        try(Connection conn =dbConnection.getConnection();
+        PreparedStatement stmt =conn.prepareStatement(sql);){
+            stmt.setString(1, productName);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                Instant createdAt = rs.getTimestamp("creation_datime").toInstant();
+                Product product = new Product(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        createdAt,
+                        null
+                );
+                products.add(product);
+            }
+            return products;
+        }
+
+    } else if (creationMin!=null) {
+        String sql= "SELECT * FROM Product WHERE creation_min<=?";
+        List<Product> products = new ArrayList<>();
+        try(Connection conn =dbConnection.getConnection();
+        PreparedStatement stmt =conn.prepareStatement(sql);){
+            stmt.setString(1, creationMin.toString());
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                Instant createdAt = rs.getTimestamp("creation_datime").toInstant();
+                Product product = new Product(
+                        rs.getInt("id"),
+                        rs.getNString("name"),
+                        createdAt,
+                        null
+                );
+                products.add(product);
+            }
+            return products;
+        }
+    } else if (creationMax!=null) {
+        String sql= "SELECT * FROM Product WHERE creation_max>=?";
+        List<Product> products = new ArrayList<>();
+        try(Connection conn =dbConnection.getConnection();
+        PreparedStatement stmt =conn.prepareStatement(sql);){
+            stmt.setString(1, creationMax.toString());
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                Instant createdAt = rs.getTimestamp("creation_datime").toInstant();
+                Product product = new Product(
+                        rs.getInt("id"),
+                        rs.getNString("name"),
+                        createdAt,
+                        null
+                );
+                products.add(product);
+            }
+            return products;
+        }
+    }
+    return null;
+    }
 }
