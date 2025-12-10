c18235431e54af9c6f14e6cec81e1213d4bad6af
nomena <hei.nomenjanahary@gmail.com>
2025-12-10 09:45:34 +0300

    finition de d une methode

--- src/main/java/hei/group/devoirprog3/DataRedriver/DataRetriever.java
+++ src/main/java/hei/group/devoirprog3/DataRedriver/DataRetriever.java
@@ -142,4 +142,89 @@ public class DataRetriever {
     }
     return null;
     }
-}
+    List<Product>
+    getProductsByCriteria(String categoryName, Instant creationMin, Instant creationMax, int page, int size, String productName)throws java.sql.SQLException{
+        int offset = (page - 1) * size;
+        if (productName!=null){
+            String sqlSelect= "SELECT * FROM Product WHERE name ILIKE ? and limit? offset?";
+            List<Product> products = new ArrayList<>();
+            try(Connection conn =dbConnection.getConnection();
+                PreparedStatement stmt =conn.prepareStatement(sqlSelect);) {
+                stmt.setString(1, productName);
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()) {
+                    Instant createdAt = rs.getTimestamp("creation_datime").toInstant();
+                    Product product = new Product(
+                            rs.getInt("id"),
+                            rs.getString("name"),
+                            createdAt,
+                            null
+                    );
+                    products.add(product);
+                }
+                return products;
+            }
+            }else if (categoryName!=null) {
+                String sqlSelects= "SELECT * FROM Product p inner join product_category c on p.id=c.id WHERE c.name ILIKE ? and limit? offset?";
+                List<Product> products3 = new ArrayList<>();
+                try(Connection conn =dbConnection.getConnection();
+                    PreparedStatement stmt =conn.prepareStatement(sqlSelects);){
+                    stmt.setString(1, productName);
+                    ResultSet rs = stmt.executeQuery();
+                    while (rs.next()){
+                        Instant createdAt = rs.getTimestamp("creation_datime").toInstant();
+                        Product product = new Product(
+                                rs.getInt("id"),
+                                rs.getString("name"),
+                                createdAt,
+                                null
+                        );
+                        products3.add(product);
+                    }
+                    return products3;
+                }
+
+            } else if (creationMin!=null) {
+                String SQL= "SELECT * FROM Product WHERE creation_min<=? and limit? offset?";
+                List<Product> products2 = new ArrayList<>();
+                try(Connection conn =dbConnection.getConnection();
+                    PreparedStatement stmt =conn.prepareStatement(SQL);){
+                    stmt.setString(1, creationMin.toString());
+                    ResultSet rs = stmt.executeQuery();
+                    while (rs.next()){
+                        Instant createdAt = rs.getTimestamp("creation_datime").toInstant();
+                        Product product = new Product(
+                                rs.getInt("id"),
+                                rs.getNString("name"),
+                                createdAt,
+                                null
+                        );
+                        products2.add(product);
+                    }
+                    return products2;
+                }
+            } else if (creationMax!=null) {
+                String sql= "SELECT * FROM Product WHERE creation_max>=? and limit? offset?";
+                List<Product> products1 = new ArrayList<>();
+                try(Connection conn =dbConnection.getConnection();
+                    PreparedStatement stmt =conn.prepareStatement(sql);){
+                    stmt.setString(1, creationMax.toString());
+                    ResultSet rs = stmt.executeQuery();
+                    while (rs.next()){
+                        Instant createdAt = rs.getTimestamp("creation_datime").toInstant();
+                        Product product = new Product(
+                                rs.getInt("id"),
+                                rs.getNString("name"),
+                                createdAt,
+                                null
+                        );
+                        products1.add(product);
+                    }
+                    return products1;
+                }
+            }
+            return null;
+
+        }
+    }
+
